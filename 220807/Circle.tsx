e6cb9b4c2a51d8257cb4ece76783f5db019c520e
hanzii <pwe1ewq@gmail.com>
2022-08-07 01:20:06 +0900

    state

--- 220807/Circle.tsx
+++ 220807/Circle.tsx
@@ -1,7 +1,5 @@
+import { useState } from "react";
 import styled from "styled-components";
-
-//optional props를 설정할 수 있음 -> ? 이거 하나로
-// 사용할 수도 있고 안 할 수도 있는 설정
 
 interface ContainerProps {
   bgColor: string;
@@ -11,9 +9,7 @@ interface ContainerProps {
 interface CircleProps {
   bgColor: string;
   borderColor?: string;
-  text?: string;
 }
-//borderColor -> undefined 될 수 있음
 
 const Container = styled.div<ContainerProps>`
   width: 200px;
@@ -21,19 +17,21 @@ const Container = styled.div<ContainerProps>`
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
   border: 1px solid ${(props) => props.borderColor};
-  // ContainerProps 에서는 borderColor가 required임! 그래서 설정해줘야 함
 `;
 
-//default 값을 지정해줄 수도 있음
-function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
-  return (
-    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
-      {text}
-    </Container>
-  );
-  //borderColor는 CircleProps에서는 undefined | stirng 이지만
-  // ContainerProps에서는 string임 --> 문제 발생!
-  //그래서 borderColor가 있으면 그걸로 설정하고 없다면 bgColor로 설정해달라는 것임
-}
+function Circle({ bgColor, borderColor }: CircleProps) {
+  // 초기값을 보고 counter type를 추측해서 만들어줌 이 경우는 number
+  const [conuter, setCounter] = useState(1);
+  setCounter(2);
+  //setCounter 안에 number가 아닌 값을 넣으면 오류가 날 것임
 
+  //만약 state 값이 string or number 둘 다 될 수 있다면
+  //useState<number|string>(0) <- 이렇게 설정해줌
+  return (
+    <Container
+      bgColor={bgColor}
+      borderColor={borderColor ?? bgColor}
+    ></Container>
+  );
+}
 export default Circle;
