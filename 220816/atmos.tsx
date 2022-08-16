fdbd9036dc3516970c1d336697c11533b5ecc38e
hanzii <pwe1ewq@gmail.com>
2022-08-17 00:25:02 +0900

    selector

--- 220816/atmos.tsx
+++ 220816/atmos.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface IToDo {
   text: string;
@@ -6,7 +6,26 @@ export interface IToDo {
   category: "TO_DO" | "DOING" | "DONE";
 }
 
+export const categoryState = atom({
+  key: "category",
+  default: "TO_DO",
+});
+
+//selector d어떤 state를 가져다가 다른 state를 만들 수 있다
+// atom은 그냥 배열
+// seletor은 atom의 output을 변경시킬 수 있다
+// state 원본은 건들지 않음!
 export const toDostate = atom<IToDo[]>({
   key: "ToDos",
   default: [],
+});
+
+//get function이 있어야 atom를 받을 수 있음
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDostate);
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
+  },
 });
--- 220816/components/ToDoList.tsx
+++ 220816/components/ToDoList.tsx
@@ -1,21 +1,32 @@
-import { useRecoilState, useRecoilValue } from "recoil";
-import { toDostate } from "../atmos";
+import React from "react";
+import { constSelector, useRecoilState, useRecoilValue } from "recoil";
+import { categoryState, toDoSelector, toDostate } from "../atmos";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDostate);
-
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
+  };
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
+      <form>
+        <select onInput={onInput}>
+          <option value="TO_DO">TO DO</option>
+          <option value="DOING">DOING</option>
+          <option value="DONE">DONE</option>
+        </select>
+      </form>
       <CreateToDo />
-      <ul>
-        {toDos.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
+      {toDos.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
     </div>
   );
 }
+
+export default ToDoList;
